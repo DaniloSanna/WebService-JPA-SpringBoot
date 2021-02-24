c2d56257e52e54de1cbf05e7618939ddf1bc91f3
Danilo Sanna <danilosanna@msn.com>
2021-02-24 12:34:43 -0300

    Service Layer, component registration

--- src/main/java/com/example/webservices/repositories/UserRepository.java
+++ src/main/java/com/example/webservices/repositories/UserRepository.java
@@ -3,6 +3,5 @@ package com.example.webservices.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.webservices.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-	
-}
+
+public interface UserRepository extends JpaRepository<User, Long>{}
